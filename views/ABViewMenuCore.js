26a6c8e1d34c0549c2cf3836d1b9c6844ee30e1b
James Duncan <james.duncan@cru.org>
2020-03-24 16:39:24 +0700

    Support submenus in menu widget

--- views/ABViewMenuCore.js
+++ views/ABViewMenuCore.js
@@ -1,230 +1,238 @@
 const ABViewWidget = require("../../platform/views/ABViewWidget");
 
 const ABViewMenuPropertyComponentDefaults = {
-	orientation: 'x',
-	buttonStyle: 'ab-menu-default',
-	menuAlignment: 'ab-menu-left',
-	menuInToolbar: 1,
-	menuPadding: 10,
-	menuTheme: "bg_gray",
-	menuPosition: "left",
-	menuTextLeft: "",
-	menuTextRight: "",
-	menuTextCenter: "",
-	// [
-	// 		{
-	//			pageId: uuid,
-	//			tabId: uuid, 
-	//			type: string, // "page", "tab"
-	//			isChecked: bool,
-	//			aliasname: string,
-	//			translations: []
-	//		}
-	// ]
-	pages: []
-}
-
+    orientation: "x",
+    buttonStyle: "ab-menu-default",
+    menuAlignment: "ab-menu-left",
+    menuInToolbar: 1,
+    menuPadding: 10,
+    menuTheme: "bg_gray",
+    menuPosition: "left",
+    menuTextLeft: "",
+    menuTextRight: "",
+    menuTextCenter: "",
+    // [
+    // 		{
+    //			pageId: uuid,
+    //			tabId: uuid,
+    //			type: string, // "page", "tab"
+    //			isChecked: bool,
+    //			aliasname: string,
+    //			translations: []
+    //		}
+    // ]
+    pages: [],
+    order: []
+};
 
 const ABMenuDefaults = {
-	key: 'menu',		// {string} unique key for this view
-	icon: 'th-large',		// {string} fa-[icon] reference for this view
-	labelKey: 'ab.components.menu' // {string} the multilingual label key for the class label
-}
+    key: "menu", // {string} unique key for this view
+    icon: "th-large", // {string} fa-[icon] reference for this view
+    labelKey: "ab.components.menu" // {string} the multilingual label key for the class label
+};
 
 module.exports = class ABViewMenuCore extends ABViewWidget {
+    /**
+     * @param {obj} values  key=>value hash of ABView values
+     * @param {ABApplication} application the application object this view is under
+     * @param {ABViewWidget} parent the ABViewWidget this view is a child of. (can be null)
+     */
+    constructor(values, application, parent, defaultValues) {
+        super(values, application, parent, defaultValues || ABMenuDefaults);
+    }
 
-	/**
-	 * @param {obj} values  key=>value hash of ABView values
-	 * @param {ABApplication} application the application object this view is under
-	 * @param {ABViewWidget} parent the ABViewWidget this view is a child of. (can be null)
-	 */
-	constructor(values, application, parent, defaultValues) {
+    static common() {
+        return ABMenuDefaults;
+    }
 
-		super(values, application, parent, defaultValues || ABMenuDefaults);
+    static defaultValues() {
+        return ABViewMenuPropertyComponentDefaults;
+    }
 
-	}
+    ///
+    /// Instance Methods
+    ///
 
-	static common() {
-		return ABMenuDefaults;
-	}
+    /**
+     * @method toObj()
+     *
+     * properly compile the current state of this ABViewLabel instance
+     * into the values needed for saving.
+     *
+     * @return {json}
+     */
+    toObj() {
+        if (this.settings.pages) {
+            this.settings.pages.forEach((page) => {
+                this.application.unTranslate(page, page, ["aliasname"]);
+            });
+        }
 
-	static defaultValues() {
-		return ABViewMenuPropertyComponentDefaults;
-	}
+        var obj = super.toObj();
+        obj.views = [];
+        return obj;
+    }
 
+    /**
+     * @method fromValues()
+     *
+     * initialze this object with the given set of values.
+     * @param {obj} values
+     */
+    fromValues(values) {
+        super.fromValues(values);
 
-	///
-	/// Instance Methods
-	///
+        this.settings.pages =
+            this.settings.pages || ABViewMenuPropertyComponentDefaults.pages;
 
-	/**
-	 * @method toObj()
-	 *
-	 * properly compile the current state of this ABViewLabel instance
-	 * into the values needed for saving.
-	 *
-	 * @return {json}
-	 */
-	toObj () {
-		
-		if (this.settings.pages) {
-			this.settings.pages.forEach(page => {
-				this.application.unTranslate(page, page, ["aliasname"]);
-			});
-		}
-		
-		var obj = super.toObj();
-		obj.views = [];
-		return obj;
-	}
+        for (var i = 0; i < this.settings.pages.length; i++) {
+            var page = this.settings.pages[i];
+            if (page instanceof Object) {
+                page.isChecked = JSON.parse(page.isChecked || false);
 
+                this.application.translate(page, page, ["aliasname"]);
+            }
+            // Compatible with old data
+            else if (typeof page == "string") {
+                this.settings.pages[i] = {
+                    pageId: page,
+                    isChecked: true
+                };
+            }
+        }
 
-	/**
-	 * @method fromValues()
-	 *
-	 * initialze this object with the given set of values.
-	 * @param {obj} values
-	 */
-	fromValues (values) {
+        this.application.translate(this, this, ["menulabel"]);
+    }
 
-		super.fromValues(values);
+    /**
+     * @method componentList
+     * return the list of components available on this view to display in the editor.
+     */
+    componentList() {
+        return [];
+    }
 
-		this.settings.pages = this.settings.pages || ABViewMenuPropertyComponentDefaults.pages;
+    ClearPagesInView(view) {
+        // clear menu items
+        if (view && view.count() > 1) {
+            view.find({}).forEach((item) => {
+                view.remove(item.id);
+            });
+        }
+    }
 
-		for (var i = 0; i < this.settings.pages.length; i++) {
+    AddPagesToView(view, pages) {
+        if (!view || !pages) return;
 
-			var page = this.settings.pages[i];
-			if (page instanceof Object) {
-				page.isChecked = JSON.parse(page.isChecked || false);
+        (pages || []).forEach((displayPage) => {
+            if (displayPage.isChecked) {
+                let existsPage = this.application.pages(
+                    (p) => p.id == displayPage.pageId,
+                    true
+                )[0];
+                if (!existsPage) return;
 
-				this.application.translate(page, page, ["aliasname"]);
-			}
-			// Compatible with old data
-			else if (typeof page == 'string') {
-				this.settings.pages[i] = {
-					pageId: page,
-					isChecked: true
-				};
-			}
-			
-		}
+                let label = this.getAliasname(displayPage);
+                // create a temporaty store for the menu item
+                var menuItem;
+                // check if page has a parent or not
+                if (displayPage.parent && displayPage.parent != "0") {
+                    // if page has a parent attempt to grab its submenu
+                    menuItem = view.getSubMenu(displayPage.parent);
+                } else {
+                    // if the page doesn't have parent use the current menu
+                    menuItem = view;
+                }
+                // if the menu item doesn't have a menu selected find it
+                if (menuItem == null) {
+                    // get the menu item
+                    var curMenu = view.getMenuItem(displayPage.parent);
+                    // create a submenu placeholder
+                    curMenu.submenu = [];
+                    // update the menu with the new submenu included
+                    view.refresh(displayPage.parent);
+                    // get the submenu we just created
+                    menuItem = view.getSubMenu(displayPage.parent);
+                }
+                if (menuItem.exists(displayPage.tabId || displayPage.pageId))
+                    return;
+                // add a new item to the menu/submenu
+                menuItem.add(
+                    {
+                        id: displayPage.tabId || displayPage.pageId,
+                        value: label,
+                        type: displayPage.type,
+                        pageId: displayPage.pageId
+                    },
+                    displayPage.position ? parseInt(displayPage.position) : 0
+                );
+            }
+        });
+    }
 
-		this.application.translate(this, this, ['menulabel']);
+    /**
+     * @method getAliasname
+     * @param pageInfo - an object in settings
+     * {
+     * 	pageId: uuid,
+     * 	tabId: uuid,
+     * 	type: string, - "page" or "tab"
+     * 	isChecked: bool,
+     * 	aliasname: string,
+     *	translations: []
+     *}
+     *
+     * @return {string}
+     */
+    getAliasname(pageInfo) {
+        var label = pageInfo.aliasname;
 
-	}
+        // if alias is empty, then find label of page or tab
+        if (
+            !label ||
+            // remove [en] or [th] etc.
+            !label.replace(/\[.{2,}\]/g, "")
+        ) {
+            // find label of the actual page
+            var page = this.application.pages(
+                (p) => p.id == pageInfo.pageId,
+                true
+            )[0];
+            if (page) {
+                // find label of the tab view
+                if (pageInfo.type == "tab") {
+                    var tabView = page.views(
+                        (v) => v.id == pageInfo.tabId,
+                        true
+                    )[0];
+                    if (tabView) {
+                        label = tabView.label;
+                    }
+                } else {
+                    label = page.label;
+                }
+            }
+        }
 
-	/**
-	 * @method componentList
-	 * return the list of components available on this view to display in the editor.
-	 */
-	componentList() {
-		return [];
-	}
+        return label;
+    }
 
-	ClearPagesInView(menu) {
-		// clear menu items
-		if (menu && menu.count() > 1) {
-			menu.find({}).forEach((item) => {
-				menu.remove(item.id);
-			});
-		}
-	}
+    copy(lookUpIds, parent) {
+        let result = super.copy(lookUpIds, parent);
 
-	AddPagesToView(menu, pages) {
+        // update ids of page's settings
+        (result.settings.pages || []).forEach((p, i) => {
+            let page = result.settings.pages[i];
 
-		if (!menu || !pages) return;
+            // Compatible with old data
+            if (typeof page == "string") {
+                result.settings.pages[i] = lookUpIds[page];
+            } else {
+                page.pageId = lookUpIds[page.pageId];
+                page.tabId = lookUpIds[page.tabId];
+            }
+        });
 
-		(pages || []).forEach(displayPage => {
-
-			if (displayPage.isChecked) {
-
-				let existsPage = this.application.pages(p => p.id == displayPage.pageId, true)[0];
-				if (!existsPage) return;
-
-				let label = this.getAliasname(displayPage);
-
-				menu.add({
-					id: displayPage.tabId || displayPage.pageId,
-					value: label,
-
-					type: displayPage.type,
-					pageId: displayPage.pageId
-				});
-			}
-
-		});
-
-	}
-
-	/**
-	 * @method getAliasname
-	 * @param pageInfo - an object in settings
-	 * { 
-	 * 	pageId: uuid,
-	 * 	tabId: uuid, 
-	 * 	type: string, - "page" or "tab"
-	 * 	isChecked: bool,
-	 * 	aliasname: string,
-	 *	translations: []
-	 *}
-	 * 
-	 * @return {string}
-	 */
-	getAliasname(pageInfo) {
-
-		var label =  pageInfo.aliasname;
-
-		// if alias is empty, then find label of page or tab
-		if (!label ||
-			// remove [en] or [th] etc.
-			!(label.replace(/\[.{2,}\]/g, ""))) {
-
-			// find label of the actual page
-			var page = this.application.pages(p => p.id == pageInfo.pageId, true)[0];
-			if (page) {
-
-				// find label of the tab view
-				if (pageInfo.type == "tab") {
-					var tabView = page.views(v => v.id == pageInfo.tabId, true)[0];
-					if (tabView) {
-						label = tabView.label;
-					}
-				}
-				else {
-					label = page.label;
-				}
-
-			}
-		}
-
-		return label;
-
-	}
-
-	copy(lookUpIds, parent) {
-
-		let result = super.copy(lookUpIds, parent);
-
-		// update ids of page's settings
-		(result.settings.pages || []).forEach((p, i) => {
-
-			let page = result.settings.pages[i];
-
-			// Compatible with old data
-			if (typeof page == 'string') {
-				result.settings.pages[i] = lookUpIds[page];
-			}
-			else {
-				page.pageId = lookUpIds[page.pageId];
-				page.tabId = lookUpIds[page.tabId];
-			}
-
-		});
-
-		return result;
-
-	}
-
-
-
-}
+        return result;
+    }
+};
