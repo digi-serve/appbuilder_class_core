c80bc47552ef13231578cc01afd8d42f02a77d2d
nohrsom <sky_wyvern@hotmail.com>
2022-11-23 11:38:27 +0700

    Fix pluck nextQLOps

--- ql/ABQLRowPluckCore.js
+++ ql/ABQLRowPluckCore.js
@@ -11,34 +11,45 @@ const ABQLValue = require("./ABQLValue.js");
 // const ABQLSet = require("./ABQLSet.js");
 const ABQLSetPluck = require("../../platform/ql/ABQLSetPluck.js");
 
+const ABQLRowUpdate = require("../../platform/ql/ABQLRowUpdate.js");
+const ABQLRowSave = require("../../platform/ql/ABQLRowSave.js");
+
 class ABQLRowPluckCore extends ABQLSetPluck {
    // Dynamic NextQLOps
    get NextQLOps() {
       let nextQLOps = [];
-      let field = this.field;
-      if (field == null) {
-         field = this.object.fieldByID(this.fieldID);
-      }
+
+      const field = this.field ?? this.object.fieldByID(this.fieldID) ?? null;
 
       // Update .NextQLOps WARN: update to static it affects to every ABQLRowPluck instances.
-      if (field) {
+      switch (field?.key) {
          // M:1 M:N connect field, then set ABQLSet to next steps
-         if (field.key == "connectObject") {
+         case "connectObject":
             if (field.settings.linkType == "many") {
                // NOTE: Could not require("./ABQLSet.js") on the top. It returns an empty object. Why ><
                const ABQLSet = require("./ABQLSet.js");
+
                nextQLOps = ABQLSet;
-            } else {
-               // return ABQLRow.js
-               nextQLOps = this.prevOP.constructor.NextQLOps;
+
+               break;
             }
-         }
-         // Normal field
-         else {
-            nextQLOps = ABQLValue;
-         }
-      } else if (this.fieldID == "_PK") {
-         nextQLOps = ABQLValue;
+
+            // return ABQLRow.js
+            nextQLOps = this.prevOP.constructor.NextQLOps.filter(
+               (NextQLOp) =>
+                  NextQLOp.key === this.constructor.key ||
+                  NextQLOp.key === ABQLRowUpdate.key
+            );
+
+            break;
+
+         default:
+            // Normal field and _PK
+            nextQLOps = this.prevOP.constructor.NextQLOps.filter(
+               (NextQLOp) => NextQLOp.key === ABQLRowSave.key
+            );
+
+            break;
       }
 
       return nextQLOps;
