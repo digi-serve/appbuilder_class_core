99f0596c678f76a1db3bf9b1761690a5e3aa21d8
wongpraatan <wongpratan@hotmail.com>
2023-09-13 14:29:11 +0700

    *Add API object to ABFactory - https://github.com/digi-serve/ab_service_appbuilder/issues/54

--- ABFactoryCore.js
+++ ABFactoryCore.js
@@ -19,6 +19,7 @@ const ABIndex = require("../platform/ABIndex");
 const ABObject = require("../platform/ABObject");
 const ABObjectExternal = require("../platform/ABObjectExternal");
 const ABObjectImport = require("../platform/ABObjectImport");
+const ABObjectApi = require("../platform/ABObjectApi");
 const ABDataCollection = require("../platform/ABDataCollection");
 const ABObjectQuery = require("../platform/ABObjectQuery");
 
@@ -659,6 +660,7 @@ class ABFactory extends EventEmitter {
          newObj = new ABObjectExternal(values, this);
       else if (values.isImported == true)
          newObj = new ABObjectImport(values, this);
+      else if (values.isAPI == true) newObj = new ABObjectApi(values, this);
       else newObj = new ABObject(values, this);
 
       /*
