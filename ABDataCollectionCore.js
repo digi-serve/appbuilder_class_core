10126572630189aec0224926153e918ba82af43c
wongpratan <wongpratan@hotmail.com>
2024-10-16 16:05:57 +0700

    *Fix parse data to DC by add some more dalay - https://github.com/digi-serve/ns_app/issues/520

--- ABDataCollectionCore.js
+++ ABDataCollectionCore.js
@@ -1906,8 +1906,12 @@ module.exports = class ABDataCollectionCore extends ABMLClass {
       return new Promise((resolve) => {
          setTimeout(async () => {
             await this.queuedParse(nextData);
-            cb?.();
-            resolve();
+
+            // Introduce a delay to ensure the UI has fully rendered the list before proceeding
+            setTimeout(async () => {
+               cb?.();
+               resolve();
+            }, 100);
          }, 15);
       });
    }
